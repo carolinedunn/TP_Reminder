3b6eec5a9dabd44ba66357e623cc0ad9eac69182
Caroline Dunn <thecarolinedunn@gmail.com>
2020-09-06 14:44:44 -0400

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,8 @@
+# Python program to execute
+# main directly
+print "Always executed"
+
+if __name__ == "__main__":
+	print "Executed when invoked directly"
+else:
+	print "Executed when imported"
---
+++ test_email.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+
+# Imports
+from datetime import datetime
+from time import sleep
+
+
+def send_simple_message():
+	return requests.post(
+		"https://api.mailgun.net/v3/YOUR_DOMAIN_NAME/messages",
+		auth=("api", "YOUR_API_KEY"),
+		data={"from": "Excited User <mailgun@YOUR_DOMAIN_NAME>",
+			"to": ["bar@example.com", "YOU@YOUR_DOMAIN_NAME"],
+			"subject": "Hello",
+			"text": "Testing some Mailgun awesomness!"})
